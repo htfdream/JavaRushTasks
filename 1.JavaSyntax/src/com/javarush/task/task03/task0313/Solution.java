e3acf410cd47a859b900f18ea00453b661de4a52
htfdream <htfdream@gmail.com>
2018-09-10 20:50:25 +0300

    0313

--- 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -7,5 +7,15 @@ package com.javarush.task.task03.task0313;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+a+c);
+        System.out.println(b+c+a);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
     }
 }
