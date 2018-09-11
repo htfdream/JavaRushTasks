28e21ca5724531f52620f06feb35d865e4b9e773
htfdream <htfdream@gmail.com>
2018-09-11 20:54:31 +0300

    0133

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0133/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0133;
+
+/* 
+Не думать о секундах…
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int secondsAfter15 = 0;
+        secondsAfter15 = Secafter();
+        System.out.println(secondsAfter15);
+    }
+    public static int Secafter(){
+        return 30*60;
+    }
+}
