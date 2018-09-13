10244218f82bda29bf4f4c0e61dc330cd7ff7014
htfdream <htfdream@gmail.com>
2018-09-13 22:18:38 +0300

    0405

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0405/Cat.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0405;
+
+/* 
+Задаем количество котов
+*/
+
+public class Cat {
+    private static int catsCount = 0;
+
+    public static void setCatsCount(int catsCount) {
+        //напишите тут ваш код
+        Cat.catsCount = catsCount;
+    }
+
+    public static void main(String[] args) {
+        setCatsCount(23);
+        System.out.println(Cat.catsCount);
+
+    }
+}
