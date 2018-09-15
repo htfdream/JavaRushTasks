46e73f0ca7adccc057f555e72dbbfb11503a3d32
htfdream <htfdream@gmail.com>
2018-09-15 22:31:19 +0300

    0435 - 0439

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task04.task0436;
+
+
+/* 
+Рисуем прямоугольник
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(buf.readLine());
+        int n = Integer.parseInt(buf.readLine());
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
+
+    }
+}
