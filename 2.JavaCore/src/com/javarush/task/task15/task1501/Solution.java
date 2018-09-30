f29f63b56b7052f1ac9b1d5c555dced28e092ab9
htfdream <htfdream@gmail.com>
2018-09-30 16:52:13 +0300

    1501 - 1505

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1501/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task15.task1501;
+
+/* 
+ООП - Расставить интерфейсы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Movable {
+        boolean isMovable();
+    }
+
+    public interface Sellable {
+        Object getAllowedAction(String name);
+    }
+
+    public interface Discountable {
+        Object getAllowedAction();
+    }
+
+    public static class Clothes implements Movable, Sellable, Discountable{
+        @Override
+        public boolean isMovable() {
+            return true;
+        }
+
+        public Clothes getAllowedAction(String name){
+            return new Clothes();
+        }
+
+        @Override
+        public Object getAllowedAction() {
+            return new Clothes();
+        }
+    }
+}
