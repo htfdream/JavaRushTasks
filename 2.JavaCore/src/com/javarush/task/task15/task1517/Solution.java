7707e26de9daf8069d5803502c5b2abd142d22f0
htfdream <htfdream@gmail.com>
2018-09-30 18:15:02 +0300

    1514 - 1518

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1517/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1517;
+
+/* 
+Статики и исключения
+*/
+
+public class Solution{
+    public static int A = 0;
+//    public Solution() throws RuntimeException{}
+    static {
+        //throw an exception here - выбросьте эксепшн тут
+        A = 1/0;
+    }
+
+    public static int B = 5;
+
+
+
+    public static void main(String[] args) {
+        System.out.println(B);
+    }
+}
